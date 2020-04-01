ed156d72aa22519eac3adf309caf3924577442ed
thierry arzal <thierryarzal353@gmail.com>
2020-04-01 11:51:03 +0200

    Slight adjustment in component Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,18 +6,15 @@ const avatar = "https://randomuser.me/api/portraits/lego/6.jpg";
 const online = true;
 
 const Contact = () => (
-
     <div className="Contact">
-    <img className="avatar" alt="avatar" src={avatar}></img>
-
-    <div>
-      <h4 className="name">{name}</h4>
-      <div className="status">
-        <div className={online ? "status-online" : "status-offline"}></div>
-        <span className="status-text">{online ? "Online" : "Offline"}</span>
-      </div>
+        <img className="avatar" src={avatar} alt="avatar"></img>
+        <div>
+            <p className="name">{name}</p>
+            <div className="status">
+                <div className={online ? "status-online" : "status-offline"} />
+                <p className="status-text">{online ? "Online" : "Offline"}</p>
+            </div>
+        </div>
     </div>
-  </div>
-
 );
 export default Contact;
