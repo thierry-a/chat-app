2a36b39c7113f86efda318e3cbddd4c106f22f88
thierry arzal <thierryarzal353@gmail.com>
2020-04-09 13:15:42 +0200

    Created ContactList component on new branch contact-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import Contact from './components/Contact';
+import "./components/Contact.css";
+import ContactList from "./components/ContactList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-      name = "Emiliano Zapata"
-      avatar = "https://randomuser.me/api/portraits/lego/6.jpg"
-      online
-      />
-      <Contact 
-      name = "Mao Zedong"
-      avatar = "https://randomuser.me/api/portraits/lego/5.jpg"
-      />
-     <Contact 
-      name = "Leo Trotsk"
-      avatar = "https://randomuser.me/api/portraits/lego/0.jpg"
-      online
-      />
+      <ContactList />
     </div>
   );
 }
+
 
 export default App;
